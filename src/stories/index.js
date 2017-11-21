3896460655f211ce68125731f5c2875590dbf2ec
Borvornsak Laoratanapong <borvornsak.la@gmail.com>
2017-11-21 20:01:45 +0700

    set background color selection in storybook

--- src/stories/index.js
+++ src/stories/index.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { linkTo } from "@storybook/addon-links";
+import backgrounds from "@storybook/addon-backgrounds";
 
-import { Button, Welcome } from '@storybook/react/demo';
+import { Button, Welcome } from "@storybook/react/demo";
+import FoodQuantityDisplay from "../components/FoodQuantityDisplay";
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf("Welcome", module).add("to Storybook", () => (
+  <Welcome showApp={linkTo("Button")} />
+));
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+storiesOf("Button", module)
+  .addDecorator(
+    backgrounds([
+      { name: "Primary Confido", value: "#535067", default: true },
+      { name: "Secondary Confido", value: "#454254" }
+    ])
+  )
+  .add("with text", () => (
+    <Button onClick={action("clicked")}>Hello Button</Button>
+  ))
+  .add("with some emoji", () => (
+    <Button onClick={action("clicked")}>😀 😎 👍 💯</Button>
+  ));
+
